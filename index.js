6388e95b992a8508f24daaa549622030dc1df30e
Donna Amos <donnacamos.developer@gmail.com>
2019-02-06 18:13:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 
 function shout(string){
   return 
-  string.toUpperCase()
+  'Hello'.toUpperCase()
 }
 
 function whisper(string){
   return 
-  string.toLowerCase()
+  'HELLO'.toLowerCase()
 }
